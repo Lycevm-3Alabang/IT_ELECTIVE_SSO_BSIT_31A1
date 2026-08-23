36604aa86160018386f6c34b3201a0f663a0fd22
ninalamo <nin.alamo@outlook.com>
2026-08-24 07:53:08 +0800

    initial commit

---
+++ Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models;
+
+[Table("TenantApps")]
+public class TenantApp
+{
+    public int Id { get; set; }
+}
+
+public class userGroup
+{
+    public int Id { get; set; }
+}
+
+public class Endpoints
+{
+    public int Id { get; set; }
+}
