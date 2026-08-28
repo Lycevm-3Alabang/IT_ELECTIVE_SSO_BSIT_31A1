902f9172eaa764a6a8fa907c5dba28c9cbfdad43
ninalamo <nin.alamo@outlook.com>
2026-08-29 07:56:31 +0800

    configured Tenant.Name to be unique

---
+++ Data/SsoDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class SsoDbContext : IdentityDbContext<IdentityUser>
+{
+    #region Ctor
+    public SsoDbContext(DbContextOptions<SsoDbContext> options) : base(options)
+    {
+    }
+
+    protected SsoDbContext()
+    {
+    }
+
+    #endregion 
+
+    #region DbSets
+
+    public DbSet<Models.TenantApp> Tenants { get; set; }
+
+    #endregion
+
+
+    #region Overrides
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        //Configure unique constraint on TenantApp.Name
+        builder.Entity<Models.TenantApp>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+
+    }
+
+    #endregion
+}
