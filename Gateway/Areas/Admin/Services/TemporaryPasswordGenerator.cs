deefbe4ef274999dacc3ac15e83c627ccd77d2e8
WspJon <realesjonnidel@gmail.com>
2026-09-05 22:07:04 +0800

    Generate random temporary password

---
+++ Gateway/Areas/Admin/Services/TemporaryPasswordGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Gateway.Areas.Admin.Services;
+
+public static class TemporaryPasswordGenerator
+{
+    private const string Chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789!@#$%";
+
+    public static string Generate(int length = 12)
+    {
+        var random = new Random();
+        var password = new char[length];
+        for (int i = 0; i < password.Length; i++)
+        {
+            password[i] = Chars[random.Next(Chars.Length)];
+        }
+        return new string(password);
+    }
+}
---
+++ Tests/TemporaryPasswordGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using Gateway.Areas.Admin.Services;
+using Xunit;
+
+namespace Data.Tests;
+
+public class TemporaryPasswordGeneratorTests
+{
+    [Fact]
+    public void Generate_DefaultLength_Returns12Characters()
+    {
+        var password = TemporaryPasswordGenerator.Generate();
+        Assert.Equal(12, password.Length);
+    }
+
+    [Fact]
+    public void Generate_CustomLength_ReturnsRequestedLength()
+    {
+        var password = TemporaryPasswordGenerator.Generate(20);
+        Assert.Equal(20, password.Length);
+    }
+
+    [Fact]
+    public void Generate_CalledTwice_ProducesDifferentValues()
+    {
+        var first = TemporaryPasswordGenerator.Generate();
+        var second = TemporaryPasswordGenerator.Generate();
+        Assert.NotEqual(first, second);
+    }
+}
