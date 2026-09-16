6cf830131fcc953d425dfc0c9660109b6dfe384f
MarcBarcinas <barcinasoct@gmail.com>
2026-09-16 14:03:33 +0800

    Updated SeedData, Program, SeedDataTests

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -16,13 +16,18 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<SsoDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/";
+    options.AccessDeniedPath = "/";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -34,6 +39,14 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
+// Area route must come BEFORE the default route, or "/Admin/..." requests
+// get matched by the default route instead (and 404, since there's no
+// "AdminController").
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
@@ -43,6 +56,5 @@ using (var scope = app.Services.CreateScope())
 {
     await SeedData.InitializeAsync(scope.ServiceProvider);
 }
-
 
 app.Run();
