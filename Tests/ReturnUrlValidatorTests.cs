b55164b3fae6d948113ab0d7683e6ae112accc88
KevinRoque1 <kcndacc@gmail.com>
2026-09-17 22:09:07 +0800

    Create ReturnUrlValidatorTests

---
+++ Tests/ReturnUrlValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using Xunit;
+
+namespace Data.Tests;
+
+public class ReturnUrlValidatorTests
+{
+    private static SsoDbContext NewInMemoryContext() =>
+        new(new DbContextOptionsBuilder<SsoDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options);
+
+    [Fact]
+    public async Task ValidateAsync_RegisteredActiveReturnUrl_ReturnsMatchingApp()
+    {
+        var context = NewInMemoryContext();
+        context.Tenants.Add(new TenantApp
+        {
+            Name = "SalesApp",
+            ReturnUrl = "https://salesapp.example.com/callback",
+            IsActive = true
+        });
+        await context.SaveChangesAsync();
+
+        var validator = new ReturnUrlValidator(context, new AuditService(context));
+
+        var result = await validator.ValidateAsync("https://salesapp.example.com/callback");
+
+        Assert.NotNull(result);
+        Assert.Equal("SalesApp", result!.Name);
+    }
+}
