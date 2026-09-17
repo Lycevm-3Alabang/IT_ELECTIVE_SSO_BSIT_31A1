0ac26c71591916fe08255ecaa2bee5d1c2fde81d
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-09-17 23:34:21 +0800

    Create GroupsController.cs

---
+++ Gateway/Areas/Admin/Controllers/GroupsController.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Gateway.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = SeedData.AdminRole)]
+public class GroupsController : Controller
+{
+    private readonly SsoDbContext _context;
+
+    public GroupsController(SsoDbContext context)
+    {
+        _context = context;
+    }
+}
